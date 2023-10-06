2b012ea539ff23fb28f11ee672d49b5925496414
sebastian tovar <sebastiantovarchavez304@gmail.com>
2023-10-06 00:35:52 -0500

    se eliminaron dos columnas de la tabla users y volvimos a cargar las migraciones

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,44 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-
 class AuthController extends Controller
 {
-    public function register(Request $request)
-{
-    $request->validate([
-        'name' => 'required|string',
-        'email' => 'required|string|email|unique:users',
-        'password' => 'required|string|min:6',
-    ]);
-
-    $user = new User([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => Hash::make($request->password),
-    ]);
-
-    $user->save();
-
-    return response()->json(['message' => 'Usuario registrado con éxito'], 201);
-}
-
-public function login(Request $request)
-{
-    $credentials = $request->validate([
-        'email' => 'required|string|email',
-        'password' => 'required|string',
-    ]);
-
-    if (!Auth::attempt($credentials)) {
-        return response()->json(['message' => 'Credenciales inválidas'], 401);
+    public function     register(Request $request){
+        
     }
-
-    $user = $request->user();
-
-    $token = $user->createToken('MyAppToken')->plainTextToken;
-
-    return response()->json(['token' => $token], 200);
-}
-
 }
