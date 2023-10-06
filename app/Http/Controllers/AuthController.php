f8771532b1742b3683e0f02fe681048fb2aa00c7
sebastian tovar <sebastiantovarchavez304@gmail.com>
2023-10-06 07:42:14 -0500

    se elimino la funcinalidad del controlador AuthController para realizar unas correcciones

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,22 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function     register(Request $request){
-        
+    public function register(Request $request)
+    {
+        return response()->json(['
+        "message" => metodo de resgistro ok']);
+    }
+    public function login(Request $request)
+    {
+    }
+
+    public function userProfile(Request $request)
+    {
+    }
+    public function logoout()
+    {
+    }
+    public function allUsers()
+    {
     }
 }
