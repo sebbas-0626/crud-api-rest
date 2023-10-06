a93f9725a35c8b33e4ba792ece01a1b16c3477b8
sebastian tovar <sebastiantovarchavez304@gmail.com>
2023-10-05 21:50:14 -0500

    creamos la migracion tareas con la columna titulo y descipcion tambien creamos nuestro controlador tareaController

---
+++ database/migrations/2023_10_06_024655_create_tareas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo'); // Columna de título (cadena de texto)
+            $table->text('descripcion'); // Columna de descripción (texto largo)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
