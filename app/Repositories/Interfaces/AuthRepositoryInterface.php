fad49acf7d9c6c88b8c05a699ef2e249de5b195c
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-09-15 20:13:26 -0500

    feat: :sparkles: INTERFACES

---
+++ app/Repositories/Interfaces/AuthRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use App\Models\User;
+
+interface AuthRepositoryInterface
+{
+    public function createUser(array $data): User;
+
+    public function findByEmail(string $email): ?User;
+
+    public function updateUser(User $user, array $data): bool;
+}
